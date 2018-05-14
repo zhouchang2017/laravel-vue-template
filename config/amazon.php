ea2ac4faf1dbc69e75e4f5f4d987afcded7fe9a8
zhouchang <290621352@qq.com>
2018-05-14 18:55:40 +0800

    update .env

--- config/amazon.php
+++ config/amazon.php
@@ -4,13 +4,13 @@ return [
     /**
      * Your access key.
      */
-    'aws_access_key_id' => env('AMAZON_AWS_ACCESS_KEY_ID', 'AKIAJFKZNIFOZ2BXUOTQ'),
+    'aws_access_key_id' => env('AMAZON_AWS_ACCESS_KEY_ID', ''),
     /**
      * Your secret key.
      */
-    'aws_secret_access_key' => env('AMAZON_AWS_SECRET_ACCESS_KEY', 'qGNNZoE5AOm5gB/Z63+2y2RRcOVk2CY09QSZwl/T'),
+    'aws_secret_access_key' => env('AMAZON_AWS_SECRET_ACCESS_KEY', ''),
     /**
      * Your seller id.
      */
-    'seller_id' =>env('AMAZON_AWS_SELLER_ID','AGLEVX6P7CNY4')
+    'seller_id' =>env('AMAZON_AWS_SELLER_ID','')
 ];
